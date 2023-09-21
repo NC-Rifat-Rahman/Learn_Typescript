e93d5e6c4bb5a41417e237b20c1e4fa067210b0a
NC-Rifat-Rahman <rahmanrifat61@gmail.com>
2023-09-21 18:07:49 +0600

    commit

---
+++ config/default.ts
@@ -0,0 +1,7 @@
+ export default {
+    port:1337,
+    host:"localhost",
+    dbUri:"mongodb+srv://rifat:123abc@cluster0.0xqlhpv.mongodb.net/rest-api-tutorial?retryWrites=true&w=majority",
+   // dbUri:"mongodb://localhost:27017/rest-api-tutorial"
+   saltWorkFactor:10,
+ }
